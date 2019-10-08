184c8dbc28484be8ecb51c5d1121a6f5bcb7569c
Philipp Dorner <pdorner@gmx.net>
2019-10-08 16:47:54 +0200

    Why is it working now?

--- int-Mo/Dice/main.js
+++ int-Mo/Dice/main.js
@@ -3,22 +3,22 @@ class Player {
     constructor(name, result) {
         this.name = name;
         this.result = 0;
-        this.rollDice()
+
     }
 
     rollDice() {
 
         let random = Math.floor(Math.random() * 6 + 1);
-      
+       
 
         this.result += Number(random);
 
-     
+       console.log(this.result);
 
     }
 }
 
-var p1 = new Player("player1", 0)
+var p1 = new Player("player1", 0);
 var p2 = new Player("player2", 0);
 
 var roll_Player1 = document.getElementById('p1-button');
@@ -29,4 +29,3 @@ var roll_Player2 = document.getElementById('p2-button');
 roll_Player2.addEventListener('click', function(){
 	p2.rollDice()
 }, false);
-console.log(p1.result);
