f8411b68ee53ae1228d16e2c08aa3d03891236ef
Philipp Dorner <pdorner@gmx.net>
2019-10-08 10:25:43 +0200

    Basic 2 Philipp

--- Basic/main.js
+++ Basic/main.js
@@ -12,3 +12,18 @@ function calcAge(){
 }
 var elementNode = document.getElementById('birthday');
       elementNode.addEventListener('input', calcAge, false);
+
+
+//Basic 2
+
+function short(){
+	 arg1 = document.getElementById("argu1").value;
+	 arg2 = document.getElementById("argu2").value;
+	document.getElementById("shortString").innerHTML = arg1.slice(0, arg2);
+
+}
+var elementNode1 = document.getElementById('argu2');
+      elementNode1.addEventListener('input', short, false);
+
+ var elementNode2 = document.getElementById('argu1');
+      elementNode2.addEventListener('input', short, false);
