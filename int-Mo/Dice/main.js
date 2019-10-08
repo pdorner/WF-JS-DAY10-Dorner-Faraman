638d55366a263c1391dfbef10f24182a3a38c710
Philipp Dorner <pdorner@gmx.net>
2019-10-08 16:38:33 +0200

    working

--- int-Mo/Dice/main.js
+++ int-Mo/Dice/main.js
@@ -2,24 +2,18 @@ sum = 0;
 class Player {
     constructor(name, result) {
         this.name = name;
-        this.result = Number(result);
-        console.log(this.result);
-
+        this.result = 0;
+        this.rollDice()
     }
+
     rollDice() {
 
-
-
-        random = Math.floor(Math.random() * 6 + 1);
-        console.log(this.result);
+        let random = Math.floor(Math.random() * 6 + 1);
+      
 
         this.result += Number(random);
 
-
-
-
-
-        console.log(this.result);
+     
 
     }
 }
@@ -28,7 +22,11 @@ var p1 = new Player("player1", 0)
 var p2 = new Player("player2", 0);
 
 var roll_Player1 = document.getElementById('p1-button');
-roll_Player1.addEventListener('click', p1.rollDice, false);
+roll_Player1.addEventListener('click', function(){
+	p1.rollDice()
+}, false);
 var roll_Player2 = document.getElementById('p2-button');
-roll_Player2.addEventListener('click', p2.rollDice, false);
+roll_Player2.addEventListener('click', function(){
+	p2.rollDice()
+}, false);
 console.log(p1.result);
